7cc37310f6cf3c3146e89a89a8c24fb72a43ed9f
Magfurul Abeer <magfurulabeer@gmail.com>
2015-11-01 03:02:48 -0500

    Update game

--- 6. Javascript and jQuery/Project 3 - Snake/scripts/script.js
+++ 6. Javascript and jQuery/Project 3 - Snake/scripts/script.js
@@ -1,17 +1,134 @@
-var grid = new Array;
+var direction = 39;
+var x = 11;
+var y = 11;
+var interval;
 var sprites = {
-	"hello": "world"
-}
-for(var i = 0; i < 160; i++) {
-	grid[i] = "";
+	"head38": "<img id='head' src='images/headup.png'>",
+	"head40": "<img id='head' src='images/headdown.png'>",
+	"head37": "<img id='head' src='images/headleft.png'>",
+	"head39": "<img id='head' src='images/headright.png'>",
+	"dead": "<img id='head' src='images/dead.png'>",
+	"redbird": "<img id='head' src='images/redbird.png'>",
+	"orangebird": "<img id='head' src='images/orangebird.png'>",
+	"greenbird": "<img id='head' src='images/greenbird.png'>",
+	"bluebird": "<img id='head' src='images/bluebird.png'>"
 }
 
-function render() {
-	for(var i = 1; i < 21; i++) {
-		for(var j = 1; j < 21; j++) {{
-			$("#screen").find(".container").append("<div class='square' id=" + i + "-" + j + "'></div>");
+var snake = ["11-11"]
+
+function createGrid() {
+	for(var i = 1; i < 22; i++) {
+		for(var j = 1; j < 22; j++) {{
+			$("#screen").find(".container").append("<div class='square " +i+"-"+j+ "'></div>");
 		}}
 	}
 }
 
-render()
+function addSprite(tile, str) {
+	$(tile).append(str);
+	$(tile).css("bottom","2px");
+}
+
+function moveSprite(a,b) {
+	if(collisionCheck(a,b)) {
+		gameOver();
+	} else {
+		eat(a,b);
+		$("." + x + "-" + y).find("img").appendTo($("." + a + "-" + b));
+		$("." + x + "-" + y).css("bottom","0px");
+		$("." + a + "-" + b).css("bottom","2px");
+	}
+	
+}
+
+function spawnFood() {
+	var blocked = false;
+	while(!blocked) {
+		var a = randomCoordinate();
+		var b = randomCoordinate();
+		if(a !== x && b !== y) {
+			var coordinate = "." + a + '-' + b;
+			addSprite(coordinate, sprites.redbird);
+			blocked = true;
+		}	 
+	}
+}
+
+function eat(a,b) {
+	var coord = "." + a + "-" + b;
+	if($(coord).children().length > 0) {
+		$(coord).find("img").remove();
+		spawnFood();
+	}
+
+}
+
+function turn(key) {
+	direction = key;
+	$("." + x + "-" + y).find("img").replaceWith(sprites["head"+direction]);
+
+}
+
+function move() {
+	switch(direction) {
+		case 37:
+			moveSprite(x,y-1);
+			y--;
+			break;
+		case 38:
+			moveSprite(x-1,y);
+			x--;
+			break;
+		case 39:
+			moveSprite(x,y+1);
+			y++;
+			break;
+		case 40:
+			moveSprite(x+1,y);
+			x++;
+			break;
+	}
+}
+
+function collisionCheck(a,b) {
+	if(a < 1 || a > 21 || b < 1 || b > 21) {
+		return true;
+	}
+	return false;
+}
+
+function gameOver() {
+	clearInterval(interval);
+	$("." + x + "-" + y).find("img").replaceWith(sprites["dead"]);
+	$(".container").css("opacity",.5)
+}
+
+function startMovement() {
+	interval = setInterval(move,100);
+}
+
+function initiate() {
+	createGrid();
+	makeBackground();
+	makeCenterPiece();
+	//Add player sprite
+	addSprite(".11-11", sprites["head39"]);
+	$(document).keydown(function(e) {
+		turn(e.which);
+	});
+	startMovement();
+	spawnFood();
+}
+
+/*
+function start() {
+	$("button").off();
+	$(".container").children().remove();
+	$(".container").removeClass("splash");
+	//$(".container").css("background-image","url(../images/green.png)")
+	initiate();
+}
+$("button").on("click",start);
+*/
+
+initiate();
---
+++ 6. Javascript and jQuery/Project 3 - Snake/scripts/tiles.js
@@ -0,0 +1,78 @@
+function setTile(name, newClass) {
+	var tiles = ["grass", "green", "yellowflower1", "yellowflower2", "pinkflower1", "pinkflower2", "grassy"];
+	for(var i in tiles) {
+		$(name).removeClass(tiles[i]);
+	}
+	$(name).addClass(newClass);
+}
+
+function makeBackground() {
+	var square = $(".square").first();
+	square.addClass(giveBackground);
+	for(var i = 0; i < $(".square").length; i++) {
+		square = square.next();		
+		var newClass = giveBackground();
+		square.addClass(newClass);
+
+	}
+}
+
+function giveBackground() {
+	var num = randomBackground();
+	if(num < 28) {
+		newClass = "grass";
+	} else if (num < 48) {
+		newClass = "green";
+	} else if (num < 50) {
+		newClass = "yellowflower1";
+	} else if (num < 52) {
+		newClass = "yellowflower2";
+	} else if (num < 54) {
+		newClass = "pinkflower1";
+	} else if (num < 56) {
+		newClass = "pinkflower2";
+	} else if (num < 577) {
+		newClass = "grassy";
+	}
+	return newClass;
+}
+
+function randomBackground() {
+	return Math.random() * (57 - 0) + 0;
+}
+
+function randomCoordinate() {
+	return Math.floor(Math.random() * (21 - 1 + 1)) + 1;
+}
+
+function makeCenterPiece() {
+	setTile(".9-10","dirttop");
+	setTile(".9-11","dirttop");
+	setTile(".9-12","dirttop");
+	setTile(".13-10","dirtbottom");
+	setTile(".13-11","dirtbottom");
+	setTile(".13-12","dirtbottom");
+	setTile(".10-13","dirtright");
+	setTile(".11-13","dirtright");
+	setTile(".12-13","dirtright");
+	setTile(".10-9","dirtleft");
+	setTile(".11-9","dirtleft");
+	setTile(".12-9","dirtleft");
+	setTile(".9-9","leaf");
+	setTile(".9-13","leaf");
+	setTile(".13-9","leaf");
+	setTile(".13-13","leaf");
+	setTile(".11-11","center");
+	setTile(".10-10","topleft");
+	setTile(".10-11","centertop");
+	setTile(".10-12","topright");
+	setTile(".12-10","bottomleft");
+	setTile(".12-11","centerbottom");
+	setTile(".12-12","bottomright");
+	setTile(".11-10","centerleft");
+	setTile(".11-12","centerright");
+}
+
+function obstacle() {
+	return false;
+}
